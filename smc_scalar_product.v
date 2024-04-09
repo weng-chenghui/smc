2fe1edbd5c6fad5c193166e24860320abe55e4d5
Takafumi Saikawa <tscompor@gmail.com>
2024-04-09 21:22:50 +0900

    refactor code blocks and generalize some properties (#10)

--- smc_scalar_product.v
+++ smc_scalar_product.v
@@ -1,128 +1,11 @@
-From mathcomp Require Import all_ssreflect all_algebra ring.
+From mathcomp.ssreflect Require Import all_ssreflect.
+From mathcomp.algebra Require Import all_algebra.
+From mathcomp.algebra_tactics Require Import ring.
+Require Import mathcomp_extra.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
-
-Section fintype_extra.
-Lemma bump0 n : bump 0 n = n.+1. Proof. by []. Qed.
-End fintype_extra.
-
-Section seq_extra.
-
-Lemma unzip1_rev A C (s : seq (A*C)) : unzip1 (rev s) = rev (unzip1 s).
-Proof. exact: map_rev. Qed.
-
-Lemma unzip2_rev A C (s : seq (A*C)) : unzip2 (rev s) = rev (unzip2 s).
-Proof. exact: map_rev. Qed.
-
-Lemma rev1 [T : Type] (x : T) : rev [:: x] = [:: x].
-Proof. by []. Qed.
-
-(* should be split int size_seq0 and size_seqS *)
-Lemma size_seq1 [T : Type] (s : seq T) :
-  size s = 1%N -> exists t, s = [:: t].
-Proof.
-case: s => //= a s.
-move/succn_inj/size0nil->.
-by exists a.
-Qed.
-
-Lemma take_zip (S T : Type) (s : seq S) (t : seq T) n :
-  take n (zip s t) = zip (take n s) (take n t).
-Proof. by elim: n s t => [|n IH] [|s1 s] [|t1 t] //=; rewrite IH. Qed.
-
-Lemma nth_zip' (S T : Type) (x : S) (y : T) s t i :
-  i < minn (size s) (size t) ->
-  nth (x, y) (zip s t) i = (nth x s i, nth y t i).
-Proof.
-move=> ?.
-rewrite -(@nth_take (minn (size s) (size t))) //.
-rewrite take_zip nth_zip; last first.
-  by rewrite !size_take_min minnAC -[RHS]minnA !minnn.
-by rewrite !nth_take.
-Qed.
-
-Lemma nth_unzip1 (S T : Type) x y (s : seq (S * T)) i :
-  nth x (unzip1 s) i = (nth (x,y) s i).1.
-Proof.
-have /orP [H|?] := leqVgt (size s) i; last by rewrite (nth_map (x,y)).
-by rewrite !nth_default ?size_map.
-Qed.
-
-Lemma nth_unzip2 (S T : Type) x y (s : seq (S * T)) i :
-  nth y (unzip2 s) i = (nth (x,y) s i).2.
-Proof.
-have /orP [H|?] := leqVgt (size s) i; last by rewrite (nth_map (x,y)).
-by rewrite !nth_default ?size_map.
-Qed.
-
-Fixpoint zipWith  (A B C: Type) (fn : A -> B -> C) (l1 : list A) (l2 : list B) : list C :=
-	match l1, l2 with
-	| [::], _ => [::]
-	| _, [::] => [::]
-	| a1 :: tl1, a2 :: tl2 => fn a1 a2 :: (zipWith fn tl1 tl2)
-	end.
-
-Lemma zipWithE (A B : Type) (f : A -> A -> B) (xx yy : list A) :
-  zipWith f xx yy = [seq f xy.1 xy.2 | xy <- zip xx yy].
-Proof.
-move: yy; elim: xx; first by case.
-by move=> x xx IH; case=> //= y yy; rewrite IH.
-Qed.
-
-Lemma size_zipWith {A B: Type} (fn : A -> A -> B) (l1 : list A) (l2 : list A) :
-  size (zipWith fn l1 l2) = minn (size l1) (size l2).
-Proof. by rewrite zipWithE size_map size_zip. Qed.
-
-Lemma big_zipWith_cat
-  [R : Type] [idx : R] (op : Monoid.com_law idx) [I : Type] (r1 r2 : seq I) 
-  (P Q : pred I) (F : I -> R) :
-  size [seq i <- r1 | P i] = size [seq i <- r2 | Q i] ->
-  \big[op/idx]_(i <- zipWith op [seq F i | i <- r1 & P i] [seq F i | i <- r2 & Q i]) i =
-    op (\big[op/idx]_(i <- r1 | P i) F i) (\big[op/idx]_(i <- r2 | Q i) F i).
-Proof.
-move=> H.
-rewrite zipWithE big_map big_split.
-rewrite -(big_map fst xpredT idfun) /= -/(unzip1 _) unzip1_zip ?size_map ?H //.
-rewrite -(big_map snd xpredT idfun) /= -/(unzip2 _) unzip2_zip ?size_map ?H //.
-by rewrite !big_map !big_filter.
-Qed.
-
-Arguments big_zipWith_cat [R idx] op [I] r1 r2 P Q F.
-
-Lemma big_zipWith_cat'
-  [R : Type] [idx : R] (op : Monoid.com_law idx) (r1 r2 : seq R) :
-  size r1 = size r2 ->
-  \big[op/idx]_(i <- zipWith op r1 r2) i =
-    op (\big[op/idx]_(i <- r1) i) (\big[op/idx]_(i <- r2) i).
-Proof.
-have:= (big_zipWith_cat op r1 r2 xpredT xpredT idfun).
-by rewrite /= !filter_predT !map_id; exact.
-Qed.
-
-Arguments big_zipWith_cat' [R idx] op r1 r2.
-
-Lemma zipWithC (A B : Type) (f : A -> A -> B) (aa bb : seq A) :
-  (forall a b, f a b = f b a) ->
-  zipWith f aa bb = zipWith f bb aa.
-Proof.  
-move: bb; elim: aa=> //=; first by case.
-by move=> a aa IH; case=> //= b bb H; rewrite H (IH _ H).
-Qed.
-
-Arguments zipWithC [A B] f.
-
-Section iteri_dep.
-Variables (A : nat -> Type) (f : forall i, A i -> A i.+1) (init : A 0).
-Fixpoint iteri_dep n : A n := if n is i.+1 then f (iteri_dep i) else init.
-
-Lemma iteri_dep_ind (P : forall {i}, A i -> Prop) :
-  (forall i (acc : A i), P acc -> P (f acc)) -> P init ->
-  forall n, P (iteri_dep n).
-Proof. by move=> IH P0; elim => //= n /IH. Qed.
-End iteri_dep.
-End seq_extra.
 
 Import GRing.Theory Num.Theory.
 
@@ -138,41 +21,93 @@ Local Open Scope ring_scope.
     https://doi.org/10.1145/844102.844125
 *)
 
-Section smc_scalar_product.
+Section datatypes.
+Variable T : Type.
 
-Variable R:ringType.
+(* Owned: genereated in or received by Alice *)
+Record alice_owned :=
+  AliceOwned {
+    Ra  : seq T;
+    Xa  : seq T;
+    X'b : seq T;
+    ra  : T;
+    recv_t   : T;
+    ya  : T;
+  }.
 
-Definition dotproduct (la lb: list R) : R :=
+(* Owned: genereated in or received by Bob *)
+Record bob_owned :=
+  BobOwned {
+    Rb  : seq T;
+    Xb  : seq T;
+    X'a : seq T;
+    rb  : T;
+    send_t : T;
+    yb  : T;
+  }.
+
+Definition SMC := list T -> list T -> (T * T * (alice_owned * bob_owned)).
+
+End datatypes.
+
+
+Section linear_algebra.
+
+Section def.
+
+Definition dotproduct (R : semiRingType) (la lb: list R) : R :=
 	foldl +%R 0 (zipWith *%R la lb).
 
-(*
-
-  The previous version `add`:
-
-    map (fun a => a.1 + a.2) (zip la lb).
-
-  Need extra hypothesis for their length,
-  and thus all related proofs need those hypothesis.
-
-  Therefore, we use this fixpoint version to save the extra work.
-*)
-
-Fixpoint add (la lb: list R) : list R :=
+Fixpoint add (R : nmodType) (la lb: list R) : list R :=
     match la, lb with
     |[::], _ => lb
     |_, [::] => la
     |a::la, b::lb => a+b :: add la lb
     end.
+(* The previous version `add`: map (fun a => a.1 + a.2) (zip la lb).
+   Need extra hypothesis for their length,
+   and thus all related proofs need those hypothesis.
+   Therefore, we use this fixpoint version to save the extra work. *)
 
-Notation "la '`*' lb" := (dotproduct la lb).
-Notation "la '`+' lb" := (add la lb).
+End def.
 
-Lemma add_cat (s1 s2 t1 t2 : seq R) :
+Local Notation "la '`*' lb" := (dotproduct la lb).
+Local Notation "la '`+' lb" := (add la lb).
+
+Lemma dot_productC (R : comSemiRingType) (aa bb : list R) : aa `* bb = bb `* aa.
+Proof.
+rewrite /dotproduct.
+elim: aa bb 0 => [| a aa IH] [| b bb] z //=.
+by rewrite mulrC.
+Qed.
+
+Lemma dot_productE (R : semiRingType) (aa bb: list R) (z: R) :
+  z + aa `* bb = foldl +%R z (zipWith *%R aa bb).
+Proof.
+rewrite /dotproduct -[in RHS](addr0 z).
+elim: aa bb z 0=> [| a aa IH] [| b bb] z z2 //=.
+by rewrite IH addrA.
+Qed.  
+
+Lemma dot_productDr (R : semiRingType) (aa bb cc : list R) :
+  aa `* (bb `+ cc) = aa `* bb + aa `* cc.
+Proof.
+rewrite /dotproduct.
+set z:={1 2}0.
+rewrite -{1}(addr0 z).
+elim: aa bb cc z 0 => [| a aa IH] [| b bb] [|c cc] z z2 //=;
+  rewrite ?IH -!dot_productE //=.
+- by rewrite !addrA.
+- by rewrite [RHS](addrC _ z2) !addrA (addrC z).
+- by rewrite mulrDr !addrA !(addrAC _ z2) !(addrAC _ (a*b)).
+Qed.
+
+Lemma add_cat (R : nmodType) (s1 s2 t1 t2 : seq R) :
   size s1 = size t1 ->
   add (s1 ++ s2) (t1 ++ t2) = add s1 t1 ++ add s2 t2.
 Proof. elim: s1 t1 => [|a aa IH] [|b bb] //= [Hs]. by rewrite IH. Qed.
 
-Lemma rev_add (s t : seq R) :
+Lemma rev_add (R : nmodType) (s t : seq R) :
   size s = size t -> rev (s `+ t) = rev s `+ rev t.
 Proof.
 rewrite -(revK s) -(revK t) !(size_rev (rev _)).
@@ -180,54 +115,40 @@ elim: (rev s) (rev t) => [|a aa IH] [|b bb] //= [Hs].
 by rewrite !rev_cons -!cats1 add_cat ?size_rev //= !cats1 !rev_rcons IH.
 Qed.
 
-Lemma take1 (s: seq R):
+Lemma take1 (R : nmodType) (s: seq R):
   (size s > 0)%N -> take 1 s = [::head 0 s].
-Proof.
-by case: s => [| ? []].
-Qed.
+Proof. by case: s => [| ? []]. Qed.
+
+End linear_algebra.
+
+Local Notation "la '`*' lb" := (dotproduct la lb).
+Local Notation "la '`+' lb" := (add la lb).
+
+
+Section smc_scalar_product.
+
+Variable R:ringType.
 
 Definition commodity_rb  (Ra Rb: list R) (ra: R): R :=
 	(Ra `* Rb) - ra.
-
-(* Owned: genereated in or received by Alice *)
-Record alice_owned :=
-  AliceOwned {
-    Ra  : seq R;
-    Xa  : seq R;
-    X'b : seq R;
-    ra  : R;
-    recv_t   : R;
-    ya  : R;
-  }.
-
-(* Owned: genereated in or received by Bob *)
-Record bob_owned :=
-  BobOwned {
-    Rb  : seq R;
-    Xb  : seq R;
-    X'a : seq R;
-    rb  : R;
-    send_t : R;
-    yb  : R;
-  }.
 
 (* Put this definition in the record will make every time we have the record,
    we need to provide the hypothesis,
    so temporarily separate it from the record.
  *)
-Definition is_alice_owned (ao: alice_owned) :=
+Definition is_alice_owned (ao: alice_owned R) :=
   ya ao = recv_t ao - (Ra ao `* X'b ao) + ra ao.
 
-Definition is_bob_owned (bo: bob_owned) :=
+Definition is_bob_owned (bo: bob_owned R) :=
   send_t bo = (Xb bo `* X'a bo) + rb bo - yb bo.
 
-
-Definition scalar_product (Ra Rb: list R) (ra rb yb: R) (Xa Xb: list R): (R * R * (alice_owned * bob_owned)) :=
-	let X'a := Xa `+ Ra in
-	let X'b := Xb `+ Rb in
-	let t := (Xb `* X'a) + rb - yb in
-	let ya := t - (Ra `* X'b) + ra in
-	(ya, yb, (AliceOwned Ra Xa X'b ra t ya, BobOwned Rb Xb X'a rb t yb)).
+Definition scalar_product (Ra Rb: list R) (ra rb yb: R) : SMC R :=
+  fun Xa Xb =>
+    let X'a := Xa `+ Ra in
+    let X'b := Xb `+ Rb in
+    let t := (Xb `* X'a) + rb - yb in
+    let ya := t - (Ra `* X'b) + ra in
+    (ya, yb, (AliceOwned Ra Xa X'b ra t ya, BobOwned Rb Xb X'a rb t yb)).
 
 Definition demo_Alice3_Bob2 : (R * R) :=
 	let Ra := [:: 9 ] in
@@ -239,61 +160,31 @@ Definition demo_Alice3_Bob2 : (R * R) :=
 	let yb := 66 in
 	(scalar_product Ra Rb ra rb yb Xa Xb).1.
 
-Definition SMC := list R -> list R -> (R * R * (alice_owned * bob_owned)).
-
-Definition is_scalar_product (sp: SMC) :=
-  forall(Xa Xb: list R),
-  (sp Xa Xb).1.1 + (sp Xa Xb).1.2 = Xa `* Xb.
+Definition is_scalar_product (sp: SMC R) :=
+  forall Xa Xb, (sp Xa Xb).1.1 + (sp Xa Xb).1.2 = Xa `* Xb.
 
 End smc_scalar_product.
 
-Eval compute in (demo_Alice3_Bob2 [ringType of int]).
+Eval compute in (demo_Alice3_Bob2 int).
 
-Notation "la '`*' lb" := (dotproduct la lb).
-Notation "la '`+' lb" := (add la lb).
 
 Section smc_scalar_product_facts.
-
-(* to prove, need comRingType, not just ringtype *)
 Variable R:comRingType.
-
-Lemma dot_productC (aa bb : list R) : aa `* bb = bb `* aa.
-Proof.
-rewrite /dotproduct.
-elim: aa bb 0 => [| a aa IH] [| b bb] z //=.
-by rewrite mulrC.
-Qed.
-
-Lemma dot_productE (aa bb: list R) (z: R): z + aa `* bb = foldl +%R z (zipWith *%R aa bb).
-Proof.
-rewrite /dotproduct -[in RHS](addr0 z).
-elim: aa bb z 0=> [| a aa IH] [| b bb] z z2 //=.
-by rewrite IH addrA.
-Qed.  
-
-Lemma dot_productDr (aa bb cc : list R) :
-  aa `* (bb `+ cc) = aa `* bb + aa `* cc.
-Proof.
-rewrite /dotproduct.
-set z:={1 2}0.
-rewrite -{1}(addr0 z).
-elim: aa bb cc z 0 => [| a aa IH] [| b bb] [|c cc] z z2 //=;
-  rewrite ?IH -!dot_productE //=; ring.
-Qed.
 
 Lemma scalar_product_correct (Ra Rb : list R) (ra yb : R) :
   let rb := commodity_rb Ra Rb ra in
   is_scalar_product (scalar_product Ra Rb ra rb yb).
 Proof.
-move=>/=Xa Xb/=.
-rewrite /commodity_rb.
-rewrite !dot_productDr.
-rewrite (dot_productC Xb Xa).
-rewrite (dot_productC Xb Ra).
-ring.
+move=> /= Xa Xb.
+rewrite  /commodity_rb /is_scalar_product /=.
+rewrite !dot_productDr (dot_productC Xb Xa) (dot_productC Xb Ra).
+(* should be just "ring." *)
+apply/eqP; rewrite eq_sym -3!subr_eq opprK !addrA.
+by rewrite !(addrAC _ (-yb)) !(addrAC _ (-ra)).
 Qed.
 
 End smc_scalar_product_facts.
+
 
 (*  
     Shen, Chih-Hao, Justin, Zhan, Tsan-Sheng, Hsu, Churn-Jung, Liau, and Da-Wei, Wang. "Scalar-product based secure two-party computation." . In 2008 IEEE International Conference on Granular Computing (pp. 556-561).2008.
@@ -301,52 +192,45 @@ End smc_scalar_product_facts.
     http://dx.doi.org/10.1109/GRC.2008.4664775
 *)
 Section zn_to_z2.
+Variable R : ringType.
 
-Let B := bool.
+Section step2_1.
+Variables (sp : SMC R) (ci xi : R * R).
 
-Definition step2_1 (sp: SMC B) (ci xi: (B * B)) : (B * B) :=
-  (sp [:: ci.1; xi.1; xi.1] [:: xi.2; ci.2; xi.2]).1.
+Local Notation alice_input := [:: ci.1; xi.1; xi.1].
+Local Notation bob_input := [:: xi.2; ci.2; xi.2].
 
-Definition step2_1_correct (sp: SMC B) (ci xi: B * B) :=
-    let alice_input := [:: ci.1; xi.1; xi.1] in
-    let bob_input := [:: xi.2; ci.2; xi.2] in
-    let t := step2_1 sp ci xi in
-    t.1 + t.2 = alice_input `* bob_input .
+Definition step2_1 : (R * R) := (sp alice_input bob_input).1.
 
-Lemma step2_1_correctP (sp: SMC B) (ci xi: B * B) :
-	is_scalar_product sp ->
-        step2_1_correct sp ci xi.
-Proof.
-move=>Hsp.
-rewrite /step2_1_correct.
-rewrite /step2_1.
-by rewrite Hsp.
-Qed.
+Definition step2_1_correct :
+  is_scalar_product sp -> step2_1.1 + step2_1.2 = alice_input `* bob_input.
+Proof. exact. Qed.
+End step2_1.
 
-Definition step2_2 (ti: B * B) (ci xi xi' : B * B) :
-  (B * B) * (B * B) :=
-	let cai' := (ci.1 * xi.1 + ti.1) in
-	let cbi' := (ci.2 * xi.2 + ti.2) in
-	let yai' := (xi'.1 + cai') in
-	let ybi' := (xi'.2 + cbi') in
-	((cai', cbi'), (yai', ybi')).
+Section step2_2.
+Variables (ti ci xi xi' : R * R).
 
-Definition step2_2_correct (ti: B * B) (ci xi xi' : B * B) :=
-    let (c, y) := step2_2 ti ci xi xi' in
+Definition step2_2 : (R * R) * (R * R) :=
+  let cai' := (ci.1 * xi.1 + ti.1) in
+  let cbi' := (ci.2 * xi.2 + ti.2) in
+  let yai' := (xi'.1 + cai') in
+  let ybi' := (xi'.2 + cbi') in
+  ((cai', cbi'), (yai', ybi')).
+
+Definition step2_2_correct :
+    let (c, y) := step2_2 in
     y.1 = c.1 + xi'.1 /\ y.2 = c.2 + xi'.2.
-
-Lemma step2_2_correctP (ti: B * B) (ci xi xi' : B * B):
-    step2_2_correct ti ci xi xi'.
-Proof. rewrite /step2_2_correct /=; split; ring. Qed.
+Proof. by split; rewrite /= addrC. Qed.
+End step2_2.
 
 End zn_to_z2.
+
 
 Section zn_to_z2_ntuple.
 
 Let B := bool.
 
 Variable (n: nat) (sps: n.-tuple (SMC B)) (xas xbs: n.+1.-tuple B).
-
 
 Notation "t '!_' i" := (tnth t i) (at level 10). (* lv9: no parenthesis; so lv10*)
 
@@ -418,9 +302,8 @@ case: acc Hacc => acc Hsz [] /=.
 move Hacc: acc Hsz => [ |[[cai cbi] [ya yb]] acc'] // Hsz.
 rewrite -{-9}Hacc /= => Hyas Hybs Hdec.
 rewrite /folder /= [in nth _ _ _](_ : i = Ordinal Hi) // -tnth_nth.
-have := step2_1_correctP (cai, cbi) (xas`_i, xbs`_i) (sps_is_sp (Ordinal Hi)).
-rewrite /step2_1_correct /=. 
-case: step2_1 => tai tbi /= Htai_tbi.
+have := step2_1_correct (cai, cbi) (xas`_i, xbs`_i) (sps_is_sp (Ordinal Hi)).
+case: step2_1 => /= tai tbi Htai_tbi.
 rewrite /acc_correct /= -Hacc.
 rewrite !(unzip1_rev,unzip2_rev) in Hyas Hybs Hdec *.
 rewrite /step2_2 /=.
@@ -457,4 +340,3 @@ by rewrite /acc_correct /= !addr0 decimal_eq0.
 Qed.
 
 End zn_to_z2_ntuple.
-
