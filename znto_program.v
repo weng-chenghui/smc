9dcc6a529433f50b897324a50c6beef1c7a93e29
Greg Weng <greg.weng@mercari.com>
2025-01-19 16:31:30 +0900

    WIP: try to make znto language

--- znto_program.v
+++ znto_program.v
@@ -32,17 +32,69 @@ Local Open Scope chap2_scope.
 Local Open Scope entropy_scope.
 Local Open Scope vec_ext_scope.
 
-Section znto.
+Section znto_def.
+
+Variable (TX : Type)(VX : Type).
+Inductive prog : Type :=
+  | Init : TX -> prog -> prog
+  | ScalarProduct : nat -> nat -> VX -> (TX -> prog) -> prog
+  | Ret : TX -> prog
+  | Finish : prog
+  | Fail : prog.
+
+End znto_def.
+
+Arguments Finish {TX}{VX}.
+Arguments Fail {TX}{VX}.
+Arguments Ret {TX}{VX}.
+
+Section znto_prog.
   
 Variable n m : nat.
-Variable T : finType.
-Variable P : R.-fdist T.
 Let TX := [the finComRingType of 'I_m.+2].
 Let VX := 'rV[TX]_n.
+
+Definition pgalice (x : VX) :=
+  Init x (
+  ScalarProduct alice bob x (fun y =>
+  ScalarProduct alice bob (x + x) (fun z => (* Don't know how to make a 'rV[TX]_n like [:: y; y+y ] *)
+  Ret z))).
 
 Let scalar_product sa sb ra yb xa xb :=
       traces (@smc_scalar_product TX VX smc_entropy_proofs.dotproduct sa sb ra yb xa xb 11).2.
 
-Check scalar_product.
+Let aresult (tr : smc_scalar_product_alice_tracesT VX) (f : TX -> TX) :=
+    let '(ya) := if tr is Some (inl ya, _, _, _, _, _)
+                then ya else 0 in f ya.
 
-End znto.
+Let bresult (tr : smc_scalar_product_alice_tracesT VX) (f : TX -> TX) :=
+    let '(yb) := if tr is Some (inl yb, _, _, _, _, _)
+                then yb else 0 in f yb.
+
+Let pgtraceT := (VX * VX * TX * TX)%type.
+
+Definition pstep (A : Type) (pg : seq (prog _ _)) (rs : seq (VX * VX * TX * TX)%type)
+  (trace : seq pgtraceT)
+  (yes no : ((prog _ _ * seq pgtraceT)%type -> A)) (i : nat) : A :=
+  let pg := nth Fail pg in
+  let p := pg i in
+  let nop := no (p, trace) in
+  let '(sa, sb, ra, yb) := nth (0, 0, 0, 0) rs i in
+  match p with
+  | ScalarProduct id1 id2 x1 f1 =>
+      if pg id2 is ScalarProduct id2 id1 x2 f2 then
+        if id1 is alice then 
+          yes (f1 (aresult (Option.bind fst (scalar_product sa sb ra yb x1 x2))), trace)
+        else
+          yes (f2 (bresult (Option.bind snd (scalar_product sa sb ra yb x2 x1))), trace)
+      else nop
+  | Init d next =>
+      yes (next, trace)
+  | Ret d =>
+      yes (Finish, trace)
+  | Finish | Fail =>
+      nop
+  end.
+
+
+End znto_prog.
